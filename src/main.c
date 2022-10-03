f1674d3dfb0969cb32958f17225212ace0b0023e
Perrine GROS <pgros@e1r6p14.clusters.42paris.fr>
2022-10-03 14:27:58 +0200

    remove changes for here_doc

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pgros <pgros@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 15:02:11 by pgros             #+#    #+#             */
-/*   Updated: 2022/09/30 18:21:50 by pgros            ###   ########.fr       */
+/*   Updated: 2022/10/03 14:16:23 by pgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@
 int main(int argc, char **argv, char **envp)
 {
 	t_parse	*parsing;
-	// char	**str_tab;
 	
 	if (argc < 5)
 	{
@@ -38,14 +37,13 @@ int main(int argc, char **argv, char **envp)
 	if (parsing == NULL)
 		return (-1);
 	
-	__print_parse(parsing);
-	(void) envp;
+	// __print_parse(parsing);
+	// (void) envp;
 	// __check_files_permissions(parsing);
 	// str_tab = __get_path_var_tab(parsing->commands->next, envp);
 	// printf("\npaths_list :\n");
 	// __print_strtab(str_tab);
-	// __execute(parsing, envp);
+	__execute(parsing, envp);
 	__free_parse(parsing);
-	// __free_strtab(str_tab);
 	return (0);
 }
